6c67315e7046efc73fbabb3395b7fe08438077f9
aedm <korteur@gmail.com>
2015-07-08 18:06:50 +0200

    Can now save stub float default values.

--- zengine/source/serialize/json/jsonserializer.cpp
+++ zengine/source/serialize/json/jsonserializer.cpp
@@ -2,6 +2,7 @@
 #include <include/dom/graph.h>
 #include <include/base/helpers.h>
 #include <include/nodes/valuenodes.h>
+#include <include/shaders/valuestubslot.h>
 #include <rapidjson/include/rapidjson/writer.h>
 #include <rapidjson/include/rapidjson/prettywriter.h>
 #include <rapidjson/include/rapidjson/stringbuffer.h>
@@ -20,71 +21,15 @@ JSONSerializer::JSONSerializer(Node* root) {
 
 string JSONSerializer::GetJSON() {
   rapidjson::StringBuffer buffer;
-  //rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
-  //writer.SetIndent(' ', 2);
-  rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+  rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+  writer.SetIndent(' ', 2);
+  //rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
   mJsonDocument.Accept(writer);
   return buffer.GetString();
 }
 
-rapidjson::Value JSONSerializer::Serialize(Node* node) {
-  rapidjson::Value v(rapidjson::kObjectType);
-
-  /// Save class type
-  NodeClass* nodeClass = NodeRegistry::GetInstance()->GetNodeClass(node);
-  v.AddMember("node", nodeClass->mClassName, *mAllocator);
-
-  /// Save node ID
-  int nodeID = mNodes.at(node);
-  v.AddMember("id", nodeID, *mAllocator);
-
-  /// Save node name
-  if (!node->GetName().empty()) {
-    v.AddMember("name", node->GetName(), *mAllocator);
-  }
-
-  /// Save slots
-  if (node->mSlots.size() > 0) {
-    rapidjson::Value slotsObject(rapidjson::kObjectType);
-    for (Slot* slot : node->mSlots) {
-      ASSERT(slot->GetName().get() != nullptr && !slot->GetName()->empty());
-      if (slot->mIsMultiSlot) {
-        rapidjson::Value connections(rapidjson::kArrayType);
-        for (Node* connectedNode : slot->GetMultiNodes()) {
-          int connectedID = mNodes.at(connectedNode);
-          connections.PushBack(connectedID, *mAllocator);
-        }
-        slotsObject.AddMember(rapidjson::Value(*slot->GetName(), *mAllocator),
-                              connections, *mAllocator);
-      } else {
-        Node* connectedNode = slot->GetNode();
-        rapidjson::Value slotValue(rapidjson::kObjectType);
-        if (IsInstanceOf<FloatSlot>(slot)) {
-          float f = static_cast<FloatSlot*>(slot)->GetDefaultValue();
-          slotValue.AddMember("default", f, *mAllocator);
-          if (node != nullptr && !slot->IsDefaulted()) {
-            int connectedID = mNodes.at(connectedNode);
-            slotValue.AddMember("id", connectedID, *mAllocator);
-          }
-          slotsObject.AddMember(rapidjson::Value(*slot->GetName(), *mAllocator),
-                                slotValue, *mAllocator);
-        } else {
-          if (connectedNode != nullptr && !slot->IsDefaulted()) {
-            int connectedID = mNodes.at(connectedNode);
-            slotsObject.AddMember(rapidjson::Value(*slot->GetName(), *mAllocator),
-                                  connectedID, *mAllocator);
-          }
-        }
-      }
-    }
-    v.AddMember("slots", slotsObject, *mAllocator);
-  }
-  return v;
-}
-
 void JSONSerializer::Traverse(Node* root) {
   mNodes[root] = ++mNodeCount;
-  mNodesByClass.emplace(NodeRegistry::GetInstance()->GetNodeClass(root), root);
   mNodesList.push_back(root);
 
   /// Traverse slots
@@ -107,34 +52,107 @@ void JSONSerializer::Traverse(Node* root) {
   }
 }
 
-void JSONSerializer::DumpDocument(Node* root) {
-  NodeClass* documentClass = NodeRegistry::GetInstance()->GetNodeClass<Document>();
-  auto range = mNodesByClass.equal_range(documentClass);
-  auto it = range.first;
-  ASSERT(IsInstanceOf<Document>(root) == (it != mNodesByClass.end()));
-  if (IsInstanceOf<Document>(root)) {
-    ASSERT(it->second == root);
-    ASSERT(++it == range.second);
-    mJsonDocument.AddMember("document", Serialize(root), *mAllocator);
-  }
-}
-
-void JSONSerializer::DumpGraphs() {
-  rapidjson::Value graphArray(rapidjson::kArrayType);
-  NodeClass* graphClass = NodeRegistry::GetInstance()->GetNodeClass<Graph>();
-  auto range = mNodesByClass.equal_range(graphClass);
-  for (auto it = range.first; it != range.second; it++) {
-    Node* node = it->second;
-    graphArray.PushBack(Serialize(node), *mAllocator);
-  }
-  mJsonDocument.AddMember("graphs", graphArray, *mAllocator);
-}
-
 void JSONSerializer::DumpNodes() {
   rapidjson::Value nodesArray(rapidjson::kArrayType);
   for (Node* node : mNodesList) {
     nodesArray.PushBack(Serialize(node), *mAllocator);
   }
   mJsonDocument.AddMember("nodes", nodesArray, *mAllocator);
+}
+
+
+rapidjson::Value JSONSerializer::Serialize(Node* node) {
+  rapidjson::Value v(rapidjson::kObjectType);
+
+  /// Save class type
+  NodeClass* nodeClass = NodeRegistry::GetInstance()->GetNodeClass(node);
+  v.AddMember("node", nodeClass->mClassName, *mAllocator);
+
+  /// Save node ID
+  int nodeID = mNodes.at(node);
+  v.AddMember("id", nodeID, *mAllocator);
+
+  /// Save node name
+  if (!node->GetName().empty()) {
+    v.AddMember("name", node->GetName(), *mAllocator);
+  }
+
+  /// Save graph position
+  if (!IsInstanceOf<Graph>(node) && !IsInstanceOf<Document>(node)) {
+    v.AddMember("position", SerializeVec2(node->GetPosition()), *mAllocator);
+  }
+
+  /// Save node content
+  if (IsInstanceOf<FloatNode>(node)) {
+    SerializeFloatNode(v, static_cast<FloatNode*>(node));
+  }
+  else {
+    SerializeGeneralNode(v, node);
+  }
+
+  return v;
+}
+
+rapidjson::Value JSONSerializer::SerializeVec2(const Vec2& vec)
+{
+  rapidjson::Value jsonObject(rapidjson::kObjectType);
+  jsonObject.AddMember("x", vec.x, *mAllocator);
+  jsonObject.AddMember("y", vec.y, *mAllocator);
+  return jsonObject;
+}
+
+
+void JSONSerializer::SerializeGeneralNode(rapidjson::Value& nodeValue, Node* node)
+{
+  /// Save slots
+  if (node->mSlots.size() > 0) {
+    rapidjson::Value slotsObject(rapidjson::kObjectType);
+    for (Slot* slot : node->mSlots) {
+      ASSERT(slot->GetName().get() != nullptr && !slot->GetName()->empty());
+
+      /// Save multislot
+      if (slot->mIsMultiSlot) {
+        rapidjson::Value connections(rapidjson::kArrayType);
+        for (Node* connectedNode : slot->GetMultiNodes()) {
+          int connectedID = mNodes.at(connectedNode);
+          connections.PushBack(connectedID, *mAllocator);
+        }
+        slotsObject.AddMember(rapidjson::Value(*slot->GetName(), *mAllocator),
+          connections, *mAllocator);
+      }
+      else {
+        Node* connectedNode = slot->GetNode();
+        rapidjson::Value slotValue(rapidjson::kObjectType);
+
+        /// Save FloatSlot
+        FloatSlot* floatSlot = dynamic_cast<FloatSlot*>(slot);
+        if (floatSlot != nullptr) {
+          float f = floatSlot->GetDefaultValue();
+          slotValue.AddMember("default", f, *mAllocator);
+          if (node != nullptr && !slot->IsDefaulted()) {
+            int connectedID = mNodes.at(connectedNode);
+            slotValue.AddMember("id", connectedID, *mAllocator);
+          }
+          slotsObject.AddMember(rapidjson::Value(*slot->GetName(), *mAllocator),
+            slotValue, *mAllocator);
+        }
+
+        /// Save all other kind of slots
+        else {
+          if (connectedNode != nullptr && !slot->IsDefaulted()) {
+            int connectedID = mNodes.at(connectedNode);
+            slotsObject.AddMember(rapidjson::Value(*slot->GetName(), *mAllocator),
+              connectedID, *mAllocator);
+          }
+        }
+      }
+    }
+    nodeValue.AddMember("slots", slotsObject, *mAllocator);
+  }
+}
+
+void JSONSerializer::SerializeFloatNode(rapidjson::Value& nodeValue, FloatNode* node)
+{
+  nodeValue.AddMember("value", node->Get(), *mAllocator);
 }
 
--- zengine/source/serialize/json/jsonserializer.h
+++ zengine/source/serialize/json/jsonserializer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <include/dom/document.h>
+#include <include/nodes/valuenodes.h>
 #include <string>
 #include <rapidjson/include/rapidjson/document.h>
 #include <unordered_map>
@@ -14,27 +15,28 @@ public:
   string GetJSON();
 
 private:
-  rapidjson::Value Serialize(Node* node);
-  
-  /// Add Document node to JSON
-  void DumpDocument(Node* root);
-
-  /// Add Graph nodes to JSON
-  void DumpGraphs();
-
-  /// Adds the rest of nodes
-  void DumpNodes();
-
   /// Collect nodes in the transitive close of root
   void Traverse(Node* root);
-  
+
+  /// Creates a JSON document from all the nodes
+  void DumpNodes();
+
+  /// Creates the JSON value for a Node
+  rapidjson::Value Serialize(Node* node);
+
+  /// Node serializers
+  void SerializeFloatNode(rapidjson::Value& nodeValue, FloatNode* node);
+  void SerializeGeneralNode(rapidjson::Value& nodeValue, Node* node);
+
+  /// Helpers
+  rapidjson::Value SerializeVec2(const Vec2& vec);
+
   /// Number of nodes to save
   int mNodeCount;
 
   /// All nodes to save
   unordered_map<Node*, int> mNodes;
   vector<Node*> mNodesList;
-  unordered_multimap<NodeClass*, Node*> mNodesByClass;
 
   rapidjson::Document mJsonDocument;
   rapidjson::Document::AllocatorType* mAllocator;
